fe87c9cef0bcd012fadfc027639afe257f823720
prannav225 <nagapranav.m@gmail.com>
2026-06-15 00:50:03 +0530

    feat: disable native Android WebView scrollbars in MainActivity

--- android/app/src/main/java/com/buckflo/app/MainActivity.java
+++ android/app/src/main/java/com/buckflo/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.buckflo.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Disable the native Android WebView scrollbars that ignore CSS
+        this.bridge.getWebView().setVerticalScrollBarEnabled(false);
+        this.bridge.getWebView().setHorizontalScrollBarEnabled(false);
+    }
+}
